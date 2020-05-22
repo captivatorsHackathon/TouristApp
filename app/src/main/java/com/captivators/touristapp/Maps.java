97322d1dbcd8d1794965f47bcc24c9267a203ed7
tupesanket1999 <tupesanket1999@gmail.com>
2020-05-22 22:03:46 +0530

    x

--- app/src/main/java/com/captivators/touristapp/Maps.java
+++ app/src/main/java/com/captivators/touristapp/Maps.java
@@ -32,7 +32,7 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(27.175164, 78.042143);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         
