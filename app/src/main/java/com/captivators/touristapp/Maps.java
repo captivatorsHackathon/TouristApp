0675748bea8d725f7e554700fb0bb76554d5f136
tupesanket1999 <tupesanket1999@gmail.com>
2020-05-22 22:26:19 +0530

    local properties addedwq

--- app/src/main/java/com/captivators/touristapp/Maps.java
+++ app/src/main/java/com/captivators/touristapp/Maps.java
@@ -30,11 +30,12 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(27.175164, 78.042143);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng tajMahal = new LatLng(27.175164, 78.042143);
+
+        mMap.addMarker(new MarkerOptions().position(tajMahal).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tajMahal));
         
     }
 }
