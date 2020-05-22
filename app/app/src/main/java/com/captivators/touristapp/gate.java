ab0cf037e4a90a603b7e34a91c7896dfab78a9c6
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2020-05-22 18:05:24 +0530

    Add files via upload

---
+++ app/app/src/main/java/com/captivators/touristapp/gate.java
@@ -0,0 +1,13 @@
+package com.captivators.touristapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gate);
+    }
+}
