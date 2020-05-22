9f67a882d3e960a13192e60201bc2a8ee8ff41a3
tupesanket1999 <tupesanket1999@gmail.com>
2020-05-22 22:39:38 +0530

    scan activity added

---
+++ app/src/main/java/com/captivators/touristapp/ImageScan.java
@@ -0,0 +1,14 @@
+package com.captivators.touristapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageScan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_scan);
+    }
+}
