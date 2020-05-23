31f2bc204fcc91ff17570ee9d679e6e849366652
tupesanket1999 <tupesanket1999@gmail.com>
2020-05-23 05:32:00 +0530

    addde chaitrali

---
+++ app/src/main/java/com/captivators/touristapp/places.java
@@ -0,0 +1,14 @@
+package com.captivators.touristapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class places extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+    }
+}
